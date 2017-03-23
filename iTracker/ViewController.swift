b7759d5ee95b7942eae692498bed234a3487053d
Simon Hällström <simonhallstrom@outlook.com>
2017-03-23 11:06:29 +0100

    Add files via upload

---
+++ iTracker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iTracker
+//
+//  Created by Simon Hällström on 2017-03-22.
+//  Copyright © 2017 Simon Hällström. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iTrackerTests/iTrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  iTrackerTests.swift
+//  iTrackerTests
+//
+//  Created by Simon Hällström on 2017-03-22.
+//  Copyright © 2017 Simon Hällström. All rights reserved.
+//
+
+import XCTest
+@testable import iTracker
+
+class iTrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
